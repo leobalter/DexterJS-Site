b5c45f68cae61df06f039f1c71bb63a0087fe19e
Leo Balter Linode <leonardo.balter@gmail.com>
2012-07-24 04:21:41 -0300

    jsonp on api.github.com

--- js/page.js
+++ js/page.js
@@ -1,3 +1,21 @@
+jQuery(function($) {
+  $.ajax( 'https://api.github.com/repos/leobalter/DexterJS', {
+    dataType: 'JSONP',
+    success: function ( data ) {
+               var repo = data.data,
+                   repoElem = $( '#repoInfo' ),
+                   repoDl = repoElem.children( 'ul' );
+                   d = new Date( repo.created_at );
+             
+               repoDl.append( '<li><b>Created: </b>' + d.getFullYear() + '/' + d.getMonth() + '/' + d.getDate() + '</li>' );
+               console.dir( data.data );                                                     
+               repoElem.show();
+             }  
+  });
+
+});
+
+
 // Twitter Button
 (function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.async=true;js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs"));
 
